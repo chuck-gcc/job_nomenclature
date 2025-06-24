8c9a4e9047b042e8a38e2af731a669944fe6a316
cb <cdricw3l@gmail.com>
2025-06-24 23:12:49 +0200

    default_push

--- offres_emploi/main.ts
+++ offres_emploi/main.ts
@@ -3,9 +3,41 @@ import dotenv from "dotenv"
 import axios from "axios";
 import fs from "fs";
 import { assert } from "console";
+import * as crypto from "crypto";
+import http from "http"
+
+class ReqStatus  
+{
+    status:number
+    status_txt:string;
+    date: Date;
+    range: [number, number, number];
+    url: string | undefined;
+    departement:string;
+
+    constructor(status:number, status_txt:string,date: Date,range: [number, number, number],departement:string,url : string  | undefined)
+    {
+        {
+
+            this.status = status;
+            this.status_txt = status_txt;
+            this.date = date;
+            this.range = range;
+            this.url =  url 
+            this.departement = departement;
+        }
+    }
+    save()
+    {
+        fs.writeFileSync(`./data/${this.departement}/status.json`, JSON.stringify(this, null, 2) + "\n", { flag: 'a' })
+    }
+}
+
 export async function sleep(ms: number): Promise<void> {
     return new Promise((resolve) => setTimeout(resolve, ms));
 }
+
+
 
 async function extract_ranges(start: number, end: number, total:number, token: string, min_d: Date, max_d:Date, departement:string)
 {
@@ -28,18 +60,36 @@ async function extract_ranges(start: number, end: number, total:number, token: s
         }
     )
 
+    
+
     //total is parametre of fonction for recursive break point.
     total = Number(res.headers['content-range'].split('/')[1]);
+    if(total > 0 && (res.status == 200 || res.status == 206))
+    {
+        /*
+            file systeme managment. departement/date/date-range-batch_size-total_size
+        */ 
+    
+        if(!fs.existsSync(`./data/${departement}`))
+            fs.mkdirSync('./data/'+departement)
+        if(!fs.existsSync(`./data/${departement}/${max_d.toISOString().split('T')[0]}`))
+            fs.mkdirSync(`./data/${departement}/${max_d.toISOString().split('T')[0]}`)
+        fs.writeFileSync(`./data/${departement}/${max_d.toISOString().split('T')[0]}/Offre-${max_d.toISOString().split('T')[0]}-${start}-${end}-${end-start}-${total}`, JSON.stringify(res.data, null, 2));
+    }
+    else
+        console.log("%c No add" , "colors")
+        
+    const status  = new ReqStatus(
 
-    /*
-        file systeme managment. departement/date/date-range-batch_size-total_size
-    */ 
-    if(!fs.existsSync(`./data/${departement}`))
-        fs.mkdirSync('./data/'+departement)
-    if(!fs.existsSync(`./data/${departement}/${max_d.toISOString().split('T')[0]}`))
-        fs.mkdirSync(`./data/${departement}/${max_d.toISOString().split('T')[0]}`)
-    fs.writeFileSync(`./data/${departement}/${max_d.toISOString().split('T')[0]}/Offre-${max_d.toISOString().split('T')[0]}-${start}-${end}-${end-start}-${total}`, JSON.stringify(res.data, null, 2));
+        res.status,
+        res.statusText,
+        max_d,
+        [start , end, total],
+        departement,
+        res.config.url,
 
+    ).save()
+    
     //dynamique ajustment for the last request;
     const add = total - end < 149 ? total - end - 1 : 149;
     // recusif call.
@@ -47,7 +97,7 @@ async function extract_ranges(start: number, end: number, total:number, token: s
 }
 
 
-async function get_job_offers_year(token: Token, departement: string, from: Date, to: Date) {
+async function departement_worker(token: Token, departement: string, from: Date, to: Date) {
 
 
     const one_day = 1000 * 60  * 60 * 24;
@@ -73,23 +123,43 @@ async function get_job_offers_year(token: Token, departement: string, from: Date
     }
 }
 
-async function main()
+async function run_extraction(token:Token, departement:string)
 {
-
-    dotenv.config();
-    const token: Token | null = await getToken();
-    if(!token)
-        return;
-
+    
     const from = new Date();
     const to = new Date();
     const day_range = 365;
 
     //from.setDate(from.getDate() - 1);
     to.setDate(from.getDate() - day_range);
+    console.log("from : " + from.toISOString() + " To: ", to.toISOString(),` for department: ${departement}`);
+    await departement_worker(token, departement, from, to)
+}
+async function main()
+{
+    dotenv.config();
+    const token: Token | null = await getToken();
+    if(!token)
+        return;
 
-    console.log("from : " + from.toISOString() + " To: ", to.toISOString()) 
-    await get_job_offers_year(token, "74", from, to)
+    const server = http.createServer((req, res) => {
+        
+        res.writeHead(200, { 'Content-Type': 'application/json' }); // Facultatif
+        res.end(JSON.stringify({ message: "Extraction démarrée", success: true }));
+        const url = new URL(req.url!,`http://${req.headers.host}`)
+        const params = url.searchParams;
+        const maxDate = params.get('maxDate');
+        const departement = params.get('departement');
+        const minDate = params.get('minDate');
+        
+        if(departement && departement != "NaN")
+            run_extraction(token, departement.toString());
+        if(departement != undefined)
+            console.log("dep: " + Number(departement[0]));
+        res.end();
+    }).listen(2022, () => { 
+        console.log("Serveur a l'ecoute");  
+    })
 }
 
 main();
