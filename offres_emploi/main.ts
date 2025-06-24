5be16b92dfd0e37367d0b318df9bf78ed31c7663
cb <cdricw3l@gmail.com>
2025-06-24 12:28:46 +0200

    default_push

---
+++ offres_emploi/main.ts
@@ -0,0 +1,77 @@
+import { Token, getToken } from "./token/token";
+import dotenv from "dotenv"
+import axios from "axios";
+import fs from "fs";
+import { assert } from "console";
+
+export async function sleep(ms: number): Promise<void> {
+    return new Promise((resolve) => setTimeout(resolve, ms));
+}
+
+async function extract_ranges(start: number, end: number, total:number, token: string, min_d: Date, max_d:Date)
+{
+
+    if(start > total || start > 3000)
+        return;
+    const res = await axios.get("https://api.francetravail.io/partenaire/offresdemploi/v2/offres/search",
+        {
+            headers:{
+                Authorization: `Bearer ${token}`
+            },
+            params:{
+                range:`${start}-${end}`,
+                departement:"74",
+                minCreationDate: min_d.toISOString().split('.')[0] + 'Z',
+                maxCreationDate: max_d.toISOString().split('.')[0] + 'Z'
+            }
+        }
+    )
+    total = Number(res.headers['content-range'].split('/')[1]);
+    console.log("extraction for range: ", start,"-",end, end-start,"of", total, "for date:", min_d.toDateString(), max_d.toDateString());
+    fs.writeFileSync(`./data/file_${start}-${end}-${end-start} of ${total}`, JSON.stringify(res.data, null, 2));
+    const add = total - end < 149 ? total - end - 1 : 149
+    await extract_ranges(end + 1, end + 1 + add,total,token, min_d, max_d);
+}
+async function get_job_offers_year(token: Token, departement: string, from: Date, to: Date) {
+
+    const today: Date = new Date();
+    const range_size =  Number( from.getDate() - to.getDate());
+
+    console.log (range_size);
+    let idx = 0
+    
+    let min_d = new Date();
+    let max_d = new Date();
+    
+    while(idx < range_size)
+    {
+        max_d.setDate(max_d.getDate() - 1);
+        min_d.setDate(max_d.getDate() - 1);
+        console.log("extraction start range ", min_d, " -", max_d); 
+        await extract_ranges(0,149,149, token.access_token, min_d, max_d);
+        await sleep(1000);
+        idx++;
+    }
+}
+
+async function main()
+{
+
+    dotenv.config();
+
+    const token: Token | null = await getToken();
+    if(!token)
+        return;
+
+    const from = new Date();
+    const to = new Date();
+    const day_range = 10;
+
+    //from.setDate(from.getDate() - 1);
+    to.setDate(from.getDate() - day_range);
+
+    await get_job_offers_year(token, "74", from, to)
+
+}
+
+main();
---
+++ offres_emploi/token/token.ts
@@ -0,0 +1,36 @@
+import axios from "axios"
+import qs from "qs"
+
+export interface Token
+{
+    access_token: string,
+    scope: string,
+    token_type: string,
+    expires_in: number
+}
+
+export async function getToken(): Promise<Token | null> {
+    const data = qs.stringify({
+        grant_type: "client_credentials",
+        client_id: process.env.CLIENT_ID,
+        client_secret: process.env.CLIENT_SECRET,
+        scope: process.env.SCOPES_API
+    });
+    console.log(process.env.CLIENT_ID);
+    console.log("Welcome to the france travail scrapping app:\n");
+    const url = "https://entreprise.pole-emploi.fr/connexion/oauth2/access_token";
+    const res = await axios.post<Token>(url, data, {
+        
+        headers:    {"Content-Type": "application/x-www-form-urlencoded"},
+        params:     {"realm": "partenaire"}
+        
+    }).catch(function (reponse){
+        console.log("error reponse status: ")
+        return (null)
+    })
+    if(res?.data)
+    {
+        return res.data
+    }
+    return null
+}
